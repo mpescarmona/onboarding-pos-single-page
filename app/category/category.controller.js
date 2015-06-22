db19c16aac8a234d2160a7d7a6f9951099decc8b
Mario Pescarmona <mpescarmona@gmail.com>
2015-06-22 17:47:48 -0300

    Update category controller

--- app/category/category.controller.js
+++ app/category/category.controller.js
@@ -3,22 +3,75 @@
 angular.module('myApp.category', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-	  .when('/category', {
-	    templateUrl: 'category/category2.html',
-	    controller: 'CategoryList'
-	  })
-	  .when('/category/:categoryId', {
-	    templateUrl: 'category/category_detail.html',
-	    controller: 'CategoryById'
+  $routeProvider.
+	  when('/category', {
+	    templateUrl: 'category/list-categories.view.html',
+	    controller: 'CategoryController'
+	  }).
+	  when('/category/create', {
+	    templateUrl: 'category/create-category.view.html',
+	    controller: 'CategoryController'
+	  }).
+	  when('/category/:categoryId', {
+	    templateUrl: 'category/view-category.view.html',
+	    controller: 'CategoryController'
+	  }).
+	  when('/category/:categoryId/edit', {
+	    templateUrl: 'category/edit-category.view.html',
+	    controller: 'CategoryController'
 	  });
 }])
 
-.controller('CategoryList', ['$scope', 'CategoryService', function ($scope, CategoryService) {
+.controller('CategoryController', ['$scope', '$route', '$routeParams','$location' , 'CategoryService', function ($scope, $route, $routeParams, $location, CategoryService) {
 	$scope.pageTitle = "Categories";
-	$scope.categories = CategoryService.query();
-}])
 
-.controller('CategoryById', ['$scope', '$route', '$routeParams', 'CategoryService', function ($scope, $route, $routeParams, CategoryService) {
-	$scope.category = CategoryService.find({categoryId: $routeParams.categoryId});
+		// Create new Category
+		$scope.create = function() {
+			// Create new Category object
+			var category = new CategoryService({
+				categoryName: this.categoryName
+			});
+
+			// Redirect after save
+			category.$save(function(response) {
+				$location.path('category');
+
+				// Clear form fields
+				$scope.categoryId = '';
+				$scope.categoryName = '';
+			});
+		};
+
+		// Remove existing Category
+		$scope.remove = function() {
+			CategoryService.remove({categoryId: $scope.category.id})
+				.$promise.then(function() {
+					for (var i in $scope.categories) {
+						if ($scope.categories[i] === $scope.category) {
+							$scope.categories.splice(i, 1);
+						}
+					}
+					$location.path('category');
+				});
+		};
+
+		// Update existing Category
+		$scope.update = function() {
+			var category = $scope.category;
+
+			CategoryService.update({categoryId: $routeParams.categoryId}, category)
+				.$promise.then(function() {
+					$location.path('category');
+				});
+		};
+
+		// Find a list of Categorys
+		$scope.find = function() {
+			$scope.categories = CategoryService.query();
+		};
+
+		// Find existing Category
+		$scope.findOne = function() {
+			$scope.category = CategoryService.get({categoryId: $routeParams.categoryId});
+		};
 }]);
